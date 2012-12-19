c5c65a99d01a5cb4a8120b8488f9d63322c497e2
ebolzan@inf.ufsm.br <ebolzan@inf.ufsm.br@8da79a60-d8bd-cfb7-bd4d-e7e64d53d2b4>
2012-12-19 03:01:31 +0000

    primeira versão

---
+++ trunk/DetectorDeadlock/src/detectordeadlock/DetectorDeadlock.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package detectordeadlock;
+
+/**
+ *
+ * @author evandro
+ */
+public class DetectorDeadlock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new NewJFrame().setVisible(true);        
+    }
+}
