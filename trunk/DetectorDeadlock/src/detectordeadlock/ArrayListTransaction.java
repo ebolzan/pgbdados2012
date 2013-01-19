d542f7c17aa5592d4e972a74e08b4a7cd08ab928
ebolzan@inf.ufsm.br <ebolzan@inf.ufsm.br@8da79a60-d8bd-cfb7-bd4d-e7e64d53d2b4>
2013-01-19 12:43:50 +0000

    unlock implementado, agora devem ser feitos testes para verificar o código!

--- trunk/DetectorDeadlock/src/detectordeadlock/ArrayListTransaction.java
+++ trunk/DetectorDeadlock/src/detectordeadlock/ArrayListTransaction.java
@@ -13,16 +13,22 @@ import javax.swing.text.html.HTMLDocument;
 
 public class ArrayListTransaction
 {    
+    //true if have deadlock in t1 or t2
     boolean t1 = false;
     boolean t2 = false;
     
+    //used to know what line there were deadlocks
+    int     line1;
+    int     line2;
+    
     ArrayList<Transaction> arraylist;
 
+   @SuppressWarnings("unchecked")
     public ArrayListTransaction() {
-        arraylist = new ArrayList();                
+            arraylist = new ArrayList();                        
     }
     
-    //insert new transaction
+    //insert new transaction    
     public void insert(Transaction t)
     {
         arraylist.add(t);        
@@ -36,6 +42,38 @@ public class ArrayListTransaction
             
         return arraylist.get(index);
     }     
+    
+    //check if have deadlock between line1 and line2
+    public boolean haveUnlock()
+    {        
+        if(line1 > line2)
+        {
+            int aux = line1;
+            line1   = line2;
+            line2   = aux;                        
+        }
+        
+        int i;        
+        for(i=line1; i<line2; i++)
+        {            
+            try
+            {
+                Transaction t = getTransaction(i);
+                
+                //if have unlock return true
+                if(t.getFunction().UNLOCK.toString().equals("UNLOCK"))
+                    return true;
+            }
+            catch(IndexOutOfBoundsException e)
+            {
+                System.out.println(e.getMessage());
+            }           
+        }
+        
+        //dont found unlock, return false
+        return false;
+    }
+    
     
     //check input data before timestant
     public boolean haveDeadlock(int id, String function, String data, int limit) 
@@ -92,18 +130,23 @@ public class ArrayListTransaction
                     {
                         System.out.println("Linha de busca "+ cont);
                         if(haveDeadlock(id, function, data, cont))
-                            this.t2 = true;
+                        {
+                            this.t2    = true;
+                            this.line2 = cont;
+                        }                            
                     }                                                                                 
                 }                    
                 else
                 {
-                    id = 1;       
-                    
+                    id = 1;                           
                     if(function != null)
                     {
                         System.out.println("Linha de busca "+ cont);
                          if(haveDeadlock( id, function, data, cont))
-                         this.t1 = true;
+                         {
+                            this.t1    = true;
+                            this.line1 = cont;
+                         }
                     }                                      
                 }                                                                    
             }            
@@ -116,8 +159,17 @@ public class ArrayListTransaction
     {
         this.checkTransaction();
         
+        //test se have deadlock
         if(t1==true && t2==true)
-            JOptionPane.showMessageDialog(null, "Ocorreu deadlock");
+        {
+            //check if between t1 and t2 haven't unlock command
+            if(haveUnlock())
+            {
+                JOptionPane.showMessageDialog(null, "Não ocorreu deadlock");
+            }
+            else
+                JOptionPane.showMessageDialog(null, "Ocorreu deadlock");
+        }
         else
             JOptionPane.showMessageDialog(null, "Não ocorreu deadlock");        
     }    
